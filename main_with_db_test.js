04e70ab6e4aecef17c210d5e3c83b76f19404afb
40995274 <yesilyurt.gok@gmail.com>
2019-02-03 04:37:19 +0300

    Reverted back to responseText

--- main_with_db_test.js
+++ main_with_db_test.js
@@ -287,7 +287,7 @@ function exportToScreen(data) {
 		if (xhr.readyState === DONE) {
 			if (xhr.status === OK) {
 				console.log(xhr.responseText); // 'This is the output.'
-				document.querySelector("#testSpan").innerText = xhr.responseXML;
+				document.querySelector("#testSpan").innerText = xhr.responseText;
 			} else {
 				console.log('Error: ' + xhr.status); // An error occurred during the request.
 			}

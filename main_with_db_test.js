2d52e4ded8b15a5398f4f0b06ca349910d778e46
40995274 <yesilyurt.gok@gmail.com>
2019-02-03 04:34:12 +0300

    Console log added

--- main_with_db_test.js
+++ main_with_db_test.js
@@ -287,6 +287,7 @@ function exportToScreen(data) {
 		if (xhr.readyState === DONE) {
 			if (xhr.status === OK) {
 				console.log(xhr.responseText); // 'This is the output.'
+				console.log(xhr.responseXML);
 				document.write(xhr.response.split("\n").join("\n"));
 			} else {
 				console.log('Error: ' + xhr.status); // An error occurred during the request.

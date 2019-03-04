ac625d163b45e60fdfc9c3da96c2e2dfbb8e58be
msteknoadam <yesilyurt.gok@gmail.com>
2019-03-04 19:02:47 +0300

    trivia_data.php origin limitation

--- trivia_data.php
+++ trivia_data.php
@@ -1,47 +1,49 @@
 <?php
-    header('Access-Control-Allow-Origin: https://gamdom.com');
-    header('Content-Type: text/plain; charset=UTF-8');
-    $servername = "localhost";
-	$username = "tekntehf_trivias_user";
-	$password = "Fo3A4QZ)*s+d";
-    $dbname = "tekntehf_trivias";
-    // Create connection
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	// Check connection
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-    }
-    $lang_change = "SET NAMES utf8mb4";
-    if (mysqli_query($conn, $lang_change)) {
-        //echo "Successful: Language change. ";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-    $sql = "SELECT name, question, answer, time, winners, chat, total FROM trivia_data";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo 
-                $row['name'].
-                "-@-".
-                $row['question'].
-                "-@-".
-                $row['answer'].
-                "-@-".
-                $row['time'].
-                "-@-".
-                $row['winners'].
-                "-@-".
-                $row['chat'].
-                "-@-".
-                $row['total'].
-                "\n";
+    if($_SERVER['HTTP_ORIGIN'] === "https://trivia.tekno.icu") {
+        header('Content-Type: text/plain; charset=UTF-8');
+        $servername = "localhost";
+        $username = "tekntehf_trivias_user";
+        $password = "Fo3A4QZ)*s+d";
+        $dbname = "tekntehf_trivias";
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
         }
+        $lang_change = "SET NAMES utf8mb4";
+        if (mysqli_query($conn, $lang_change)) {
+            //echo "Successful: Language change. ";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        $sql = "SELECT name, question, answer, time, winners, chat, total FROM trivia_data";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo 
+                    $row['name'].
+                    "-@-".
+                    $row['question'].
+                    "-@-".
+                    $row['answer'].
+                    "-@-".
+                    $row['time'].
+                    "-@-".
+                    $row['winners'].
+                    "-@-".
+                    $row['chat'].
+                    "-@-".
+                    $row['total'].
+                    "\n";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
     } else {
-        echo "0 results";
+        echo "This page can only be accessed from trivia.tekno.icu"
     }
-    $conn->close();
-    
 ?>

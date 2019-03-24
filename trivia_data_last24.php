39daa2fceaf1957ed677d7ea18a895c708deeec0
msteknoadam <yesilyurt.gok@gmail.com>
2019-03-24 14:04:08 +0300

    Added last 24 hour trivia data GET page

---
+++ trivia_data_last24.php
@@ -0,0 +1,45 @@
+<?php
+    header('Content-Type: text/plain; charset=UTF-8');
+    $servername = "localhost";
+    $username = "tekntehf_trivias_user";
+    $password = "Fo3A4QZ)*s+d";
+    $dbname = "tekntehf_trivias";
+    $minTimeStamp = microtime() - (24 * 60 * 60 * 1000);
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $lang_change = "SET NAMES utf8mb4";
+    if (mysqli_query($conn, $lang_change)) {
+        //echo "Successful: Language change. ";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    $sql = "SELECT name, question, answer, time, winners, chat, total FROM trivia_data WHERE time > $minTimeStamp";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo 
+                $row['name'].
+                "-@-".
+                $row['question'].
+                "-@-".
+                $row['answer'].
+                "-@-".
+                $row['time'].
+                "-@-".
+                $row['winners'].
+                "-@-".
+                $row['chat'].
+                "-@-".
+                $row['total'].
+                "\n";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>

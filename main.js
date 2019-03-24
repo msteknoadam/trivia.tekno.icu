118707091ebe75dedb651971403cd5499211dc3a
msteknoadam <yesilyurt.gok@gmail.com>
2019-03-24 15:32:39 +0300

    All time bodge-fix try

--- main.js
+++ main.js
@@ -23,6 +23,9 @@ Number.prototype.toMoney = function(decimals, decimal_sep, thousands_sep) {
 	);
 };
 
+window.renderedLast24Mods = [];
+window.renderedAllTimeMods = [];
+
 /* Getting trivia datas from the database */
 fetch('trivia_data_last24.php')
 	.then(response => {
@@ -209,4 +212,181 @@ function exportLast24ToScreen(data) {
 	).innerHTML = `Today, we have given away a total of <span style="font-weight: bold;">${String(
 		todayTotalCoins.toMoney(0, '', ' ')
 	)}</span> coins.`;
+}
+
+document.querySelector('#previousShow').addEventListener('click', e => {
+	e.preventDefault();
+	if (!window.allTimeTrivias) {
+		fetch('trivia_data_alltime.php')
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				window.allTimeTrivias = data;
+				exportAllTimeToScreen(data);
+			});
+	}
+});
+
+function exportAllTimeToScreen(data) {
+	/* Defining global variables */
+	var leaderboardAllTimeData = {},
+		sortedLeaderboardAllTimeData = [];
+
+	/* Processing each trivia */
+	trivias.forEach(function(trivia) {
+		if (Object.keys(trivia).length == 7) {
+			/* Calculation of all time trivias START */
+			allTimeTotalCoins += Number(trivia.totalAmount);
+			var winnersArray = trivia.winners.split(',');
+			if (!isNaN(trivia.totalAmount)) {
+				winnersArray.forEach(winner => {
+					/* If the user has already won a trivia, add the current trivia's reward to user's previous amount. Otherwise, create a new object key-value pair for that user. */
+					if (leaderboardAllTimeData[winner])
+						leaderboardAllTimeData[winner] += Math.floor(trivia.totalAmount / winnersArray.length);
+					// Using floor so it will prevent retarded numbers (tho this is not how it should be)
+					else {
+						leaderboardAllTimeData[winner] = Math.floor(trivia.totalAmount / winnersArray.length); // Using floor so it will prevent retarded numbers (tho this is not how it should be)
+					}
+				});
+				var sortable = [];
+				for (var user in leaderboardAllTimeData) {
+					sortable.push([user, leaderboardAllTimeData[user]]);
+				}
+				sortable.sort((a, b) => {
+					return b[1] - a[1];
+				});
+				sortedLeaderboardAllTimeData = sortable;
+			}
+			var allTimeexists = false,
+				allTimethIndex = 0;
+
+			if (document.querySelector('#allTimeTrivias').querySelectorAll('th').length > 0) {
+				for (var i = 0; i < document.querySelector('#allTimeTrivias').querySelectorAll('th').length; i++) {
+					title = document.querySelector('#allTimeTrivias').querySelectorAll('th')[i];
+					if (title.innerText.split('\n')[0] == trivia.hoster) {
+						allTimeexists = true;
+						allTimethIndex = i;
+					}
+				}
+			}
+
+			if (allTimeexists == true) {
+				var allTimebutton = document.createElement('button');
+				allTimebutton.className = 'triviaButton';
+				allTimebutton.innerText = new Date(Number(trivia.timeStamp)).toLocaleString();
+				allTimebutton.addEventListener('click', function() {
+					//alert(`Chat: ${chatName}\nQuestion: ${question}\nAnswer: ${answer}\nWinners: ${winners}\nTotal coins given out: ${totalAmount}`);
+					if (allTimebutton.parentNode.querySelector('div').className.includes('hidden')) {
+						allTimebutton.parentNode.querySelector(
+							'div'
+						).className = allTimebutton.parentNode
+							.querySelector('div')
+							.className.replace('hidden', 'visible');
+						if (
+							allTimebutton.parentNode.querySelector('div').getBoundingClientRect().left >
+							document.body.offsetWidth - 500
+						)
+							allTimebutton.parentNode.querySelector('div').className += ' overWidthRight';
+					} else allTimebutton.parentNode.querySelector('div').className = allTimebutton.parentNode.querySelector('div').className.replace('visible', 'hidden');
+				});
+				var div = document.createElement('div');
+				div.className = 'triviaDetail hidden';
+				var span = document.createElement('span');
+				span.innerText = `Chat: ${trivia.chatName}\nQuestion: ${trivia.question}\nAnswer: ${
+					trivia.answer
+				}\nWinners: ${trivia.winners}\nTotal coins given out: ${trivia.totalAmount}`;
+				div.append(span);
+				var triviaButtonDiv = document.createElement('div');
+				triviaButtonDiv.className = 'triviaButtonDiv';
+				triviaButtonDiv.appendChild(allTimebutton);
+				triviaButtonDiv.appendChild(div);
+				document
+					.querySelector('#allTimeTrivias')
+					.querySelectorAll('td')
+					[allTimethIndex].appendChild(triviaButtonDiv);
+				document
+					.querySelector('#allTimemodList')
+					.querySelectorAll('th')
+					[allTimethIndex].querySelector('.total').innerText =
+					Number(
+						document
+							.querySelector('#allTimemodList')
+							.querySelectorAll('th')
+							[allTimethIndex].querySelector('.total').innerText
+					) + Number(trivia.totalAmount);
+			} else {
+				var th = document.createElement('th');
+				th.innerHTML = `${trivia.hoster}<br /><span class="total">${trivia.totalAmount}</span>`;
+				document
+					.querySelector('#allTimeTrivias')
+					.querySelector('#allTimemodList')
+					.appendChild(th);
+				var td = document.createElement('td');
+				document
+					.querySelector('#allTimeTrivias')
+					.querySelector('#allTimetriviaList')
+					.appendChild(td);
+				for (var i = 0; i < document.querySelector('#allTimeTrivias').querySelectorAll('th').length; i++) {
+					title = document.querySelector('#allTimeTrivias').querySelectorAll('th')[i];
+					if (title.innerText.split('\n')[0] == trivia.hoster) {
+						allTimeexists = true;
+						allTimethIndex = i;
+					}
+				}
+				var allTimebutton = document.createElement('button');
+				allTimebutton.className = 'triviaButton';
+				allTimebutton.innerText = new Date(Number(trivia.timeStamp)).toLocaleString();
+				allTimebutton.addEventListener('click', () => {
+					//alert(`Chat: ${chatName}\nQuestion: ${question}\nAnswer: ${answer}\nWinners: ${winners}\nTotal coins given out: ${totalAmount}`);
+					if (allTimebutton.parentNode.querySelector('div').className.includes('hidden')) {
+						allTimebutton.parentNode.querySelector(
+							'div'
+						).className = allTimebutton.parentNode
+							.querySelector('div')
+							.className.replace('hidden', 'visible');
+						if (
+							allTimebutton.parentNode.querySelector('div').getBoundingClientRect().left >
+							document.body.offsetWidth - 500
+						)
+							allTimebutton.parentNode.querySelector('div').className += ' overWidthRight';
+					} else
+						allTimebutton.parentNode.querySelector(
+							'div'
+						).className = allTimebutton.parentNode
+							.querySelector('div')
+							.className.replace('visible', 'hidden');
+				});
+				var div = document.createElement('div');
+				div.className = 'triviaDetail hidden';
+				var span = document.createElement('span');
+				span.innerText = `Chat: ${trivia.chatName}\nQuestion: ${trivia.question}\nAnswer: ${
+					trivia.answer
+				}\nWinners: ${trivia.winners}\nTotal coins given out: ${trivia.totalAmount}`;
+				div.append(span);
+				var triviaButtonDiv = document.createElement('div');
+				triviaButtonDiv.className = 'triviaButtonDiv';
+				triviaButtonDiv.appendChild(allTimebutton);
+				triviaButtonDiv.appendChild(div);
+				document
+					.querySelector('#allTimeTrivias')
+					.querySelectorAll('td')
+					[allTimethIndex].appendChild(triviaButtonDiv);
+			}
+			/* Calculation of all time trivias END */
+		}
+	});
+
+	/* Add the sorted datas into the actual HTML page */
+	sortedLeaderboardAllTimeData.forEach(userData => {
+		var tr = document.createElement('tr'),
+			tdUsername = document.createElement('td'),
+			tdCoinsWon = document.createElement('td');
+
+		tdUsername.innerText = userData[0];
+		tdCoinsWon.innerText = String(userData[1]);
+		tr.append(tdCoinsWon);
+		tr.prepend(tdUsername);
+		document.querySelector('#leaderboard24').append(tr);
+	});
 }
